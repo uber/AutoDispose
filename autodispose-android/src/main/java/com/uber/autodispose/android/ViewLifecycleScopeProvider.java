6ef9bdc8424e04507681ecc84bf0b21901d817ad
Zac Sweers <pandanomic@gmail.com>
2017-02-01 18:09:15 +0700

    Rework lifecycle to match RxBinding/RxJava 2 style with custom observable
    Note that we need to actually send the exception to onError() here rather than just throwing, as RxJava can't catch exceptions in subscribe().

--- autodispose-android/src/main/java/com/uber/autodispose/android/ViewLifecycleScopeProvider.java
+++ autodispose-android/src/main/java/com/uber/autodispose/android/ViewLifecycleScopeProvider.java
@@ -20,14 +20,8 @@ import android.view.View;
 import com.uber.autodispose.LifecycleScopeProvider;
 import com.uber.autodispose.OutsideLifecycleException;
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.Cancellable;
 import io.reactivex.functions.Function;
 
-import static com.uber.autodispose.AutoDisposeUtil.checkNotNull;
-import static com.uber.autodispose.android.AutoDisposeAndroidUtil.isMainThread;
 import static com.uber.autodispose.android.ViewLifecycleEvent.DETACH;
 
 public class ViewLifecycleScopeProvider implements LifecycleScopeProvider<ViewLifecycleEvent> {
@@ -53,58 +47,15 @@ public class ViewLifecycleScopeProvider implements LifecycleScopeProvider<ViewLi
    * @return a {@link LifecycleScopeProvider} against this view.
    */
   public static LifecycleScopeProvider from(View view) {
-    checkNotNull(view, "view == null");
+    if (view == null) {
+      throw new NullPointerException("view == null");
+    }
     return new ViewLifecycleScopeProvider(view);
   }
 
   private ViewLifecycleScopeProvider(final View view) {
     this.view = view;
-    lifecycle = Observable.create(new ObservableOnSubscribe<ViewLifecycleEvent>() {
-      @Override public void subscribe(final ObservableEmitter<ViewLifecycleEvent> e)
-          throws Exception {
-        if (!isMainThread()) {
-          throw new IllegalStateException("Views can only be bound to on the main thread!");
-        }
-
-        if (AutoDisposeAndroidUtil.isAttached(view)) {
-          // Emit the last event, like a behavior subject
-          e.onNext(ViewLifecycleEvent.ATTACH);
-        }
-
-        final View.OnAttachStateChangeListener listener = new View.OnAttachStateChangeListener() {
-          @Override public void onViewAttachedToWindow(View view1) {
-            e.onNext(ViewLifecycleEvent.ATTACH);
-          }
-
-          @Override public void onViewDetachedFromWindow(View view1) {
-            e.onNext(DETACH);
-          }
-        };
-
-        e.setCancellable(new Cancellable() {
-          @Override public void cancel() throws Exception {
-            // A "main thread cancellable"
-            if (isMainThread()) {
-              onCancel();
-            } else {
-              AndroidSchedulers.mainThread()
-                  .createWorker()
-                  .schedule(new Runnable() {
-                    @Override public void run() {
-                      onCancel();
-                    }
-                  });
-            }
-          }
-
-          private void onCancel() {
-            view.removeOnAttachStateChangeListener(listener);
-          }
-        });
-
-        view.addOnAttachStateChangeListener(listener);
-      }
-    });
+    lifecycle = new ViewAttachEventsObservable(view);
   }
 
   @Override public Observable<ViewLifecycleEvent> lifecycle() {
