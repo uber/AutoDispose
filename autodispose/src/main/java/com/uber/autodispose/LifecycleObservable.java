347b4eab1e2858415f0970d181c7822961387c54
Zac Sweers <pandanomic@gmail.com>
2017-02-08 13:15:26 -0800

    Back with behavior subject and expose top level API

--- autodispose/src/main/java/com/uber/autodispose/LifecycleObservable.java
+++ autodispose/src/main/java/com/uber/autodispose/LifecycleObservable.java
@@ -18,8 +18,12 @@ package com.uber.autodispose;
 
 import io.reactivex.Observable;
 import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Function;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.subjects.BehaviorSubject;
+import io.reactivex.subjects.Subject;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * An Observable whose behavior is similar to that of a {@link BehaviorSubject} but read-only, for
@@ -37,6 +41,9 @@ public final class LifecycleObservable<T> extends Observable<T> {
         }
       };
 
+  private final AtomicReference<Disposable> subjectDisposable = new AtomicReference<>();
+  private volatile BehaviorSubject<T> subject = BehaviorSubject.create();
+  private volatile Subject<T> serializedSubject = subject.toSerialized();
   private final Observable<T> actual;
 
   /**
@@ -61,6 +68,116 @@ public final class LifecycleObservable<T> extends Observable<T> {
   }
 
   @Override protected void subscribeActual(Observer<? super T> observer) {
-    actual.subscribe(observer);
+    // Subscribe the subject to the observer, track disposal so we can clear the behavior
+    // subscription later.
+    subject.subscribe(new LifecycleObserver(observer) {
+      @Override void onDispose() {
+        Disposable d = subjectDisposable.get();
+        if (d != null && !d.isDisposed()) {
+          d.dispose();
+        }
+      }
+    });
+    subjectDisposable.set(actual.subscribeWith(new DisposableObserver<T>() {
+      @Override public void onNext(T t) {
+        serializedSubject.onNext(t);
+      }
+
+      @Override public void onError(Throwable e) {
+        serializedSubject.onError(e);
+      }
+
+      @Override public void onComplete() {
+        serializedSubject.onComplete();
+      }
+    }));
+  }
+
+  /**
+   * Passthrough to {@link BehaviorSubject#getThrowable()}
+   */
+  public Throwable getThrowable() {
+    return subject.getThrowable();
+  }
+
+  /**
+   * Passthrough to {@link BehaviorSubject#getValue()}
+   */
+  public T getValue() {
+    return subject.getValue();
+  }
+
+  /**
+   * Passthrough to {@link BehaviorSubject#getValues()}
+   */
+  public Object[] getValues() {
+    return subject.getValues();
+  }
+
+  /**
+   * Passthrough to {@link BehaviorSubject#hasComplete()}
+   */
+  public boolean hasComplete() {
+    return subject.hasComplete();
+  }
+
+  /**
+   * Passthrough to {@link BehaviorSubject#hasThrowable()}
+   */
+  public boolean hasThrowable() {
+    return subject.hasThrowable();
+  }
+
+  /**
+   * Passthrough to {@link BehaviorSubject#getValues(Object[])}
+   */
+  public T[] getValues(T[] array) {
+    return subject.getValues(array);
+  }
+
+  /**
+   * Passthrough to {@link BehaviorSubject#hasValue()}
+   */
+  public boolean hasValue() {
+    return subject.hasValue();
+  }
+
+  abstract class LifecycleObserver implements Observer<T>, Disposable {
+
+    private final AtomicReference<Disposable> disposableRef = new AtomicReference<>();
+    final Observer<? super T> actual;
+
+    private LifecycleObserver(Observer<? super T> actual) {
+      this.actual = actual;
+    }
+
+    @Override public void onNext(T t) {
+      actual.onNext(t);
+    }
+
+    @Override public void onError(Throwable e) {
+      actual.onError(e);
+    }
+
+    @Override public void onComplete() {
+      actual.onComplete();
+    }
+
+    @Override public void dispose() {
+      AutoDisposableHelper.dispose(disposableRef);
+      onDispose();
+    }
+
+    @Override public boolean isDisposed() {
+      return AutoDisposableHelper.isDisposed(disposableRef.get());
+    }
+
+    abstract void onDispose();
+
+    @Override public void onSubscribe(Disposable d) {
+      if (AutoDisposableHelper.setOnce(disposableRef, d)) {
+        actual.onSubscribe(this);
+      }
+    }
   }
 }
