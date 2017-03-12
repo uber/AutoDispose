47a586049afaf753e9f0d47985c9136e21ce8c7d
Zac Sweers <pandanomic@gmail.com>
2017-03-12 03:45:34 -0700

    Add autodispose kotlin extensions

--- autodispose-kotlin/src/main/kotlin/com/uber/autodispose/kotlin/autodispose.kt
+++ autodispose-kotlin/src/main/kotlin/com/uber/autodispose/kotlin/autodispose.kt
@@ -1,138 +1,149 @@
+/*
+ * Copyright (c) 2017. Uber Technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 @file:Suppress("NOTHING_TO_INLINE")
 
 package com.uber.autodispose.kotlin
 
-import com.uber.autodispose.AutoDispose
-import com.uber.autodispose.LifecycleProvider
-import com.uber.autodispose.internal.AutoDisposeUtil
-import com.uber.autodispose.internal.AutoDisposeUtil.emptyConsumer
+import com.uber.autodispose.CompletableScoper
+import com.uber.autodispose.CompletableSubscribeProxy
+import com.uber.autodispose.FlowableScoper
+import com.uber.autodispose.FlowableSubscribeProxy
+import com.uber.autodispose.LifecycleScopeProvider
+import com.uber.autodispose.MaybeScoper
+import com.uber.autodispose.MaybeSubscribeProxy
+import com.uber.autodispose.ObservableScoper
+import com.uber.autodispose.ObservableSubscribeProxy
+import com.uber.autodispose.ScopeProvider
+import com.uber.autodispose.SingleScoper
+import com.uber.autodispose.SingleSubscribeProxy
 import io.reactivex.Completable
-import io.reactivex.CompletableObserver
 import io.reactivex.Flowable
 import io.reactivex.Maybe
-import io.reactivex.MaybeObserver
 import io.reactivex.Observable
-import io.reactivex.Observer
 import io.reactivex.Single
-import io.reactivex.SingleObserver
 import io.reactivex.annotations.CheckReturnValue
-import io.reactivex.disposables.Disposable
-import io.reactivex.functions.Action
-import io.reactivex.functions.BiConsumer
-import io.reactivex.functions.Consumer
-import org.reactivestreams.Subscriber
-import org.reactivestreams.Subscription
 
+/**
+ * Extension that proxies to [Flowable.to] + [FlowableScoper]'s [Maybe] constructor.
+ */
 @CheckReturnValue
-inline fun <T> Observer<T>.disposeOn(provider: LifecycleProvider<Any>): Observer<T>
-    = AutoDispose.observable(provider).around(this)
+inline fun <T> Flowable<T>.autoDisposeWith(scope: Maybe<*>): FlowableSubscribeProxy<T>
+    = this.to(FlowableScoper<T>(scope))
 
+/**
+ * Extension that proxies to [Observable.to] + [ObservableScoper]'s [Maybe] constructor.
+ */
 @CheckReturnValue
-inline fun <T> SingleObserver<T>.disposeOn(provider: LifecycleProvider<Any>): SingleObserver<T>
-    = AutoDispose.single(provider).around(this)
+inline fun <T> Observable<T>.autoDisposeWith(scope: Maybe<*>): ObservableSubscribeProxy<T>
+    = this.to(ObservableScoper<T>(scope))
 
+/**
+ * Extension that proxies to [Single.to] + [SingleScoper]'s [Maybe] constructor.
+ */
 @CheckReturnValue
-inline fun <T> MaybeObserver<T>.disposeOn(provider: LifecycleProvider<Any>): MaybeObserver<T>
-    = AutoDispose.maybe(provider).around(this)
+inline fun <T> Single<T>.autoDisposeWith(scope: Maybe<*>): SingleSubscribeProxy<T>
+    = this.to(SingleScoper<T>(scope))
 
+/**
+ * Extension that proxies to [Maybe.to] + [MaybeScoper]'s [Maybe] constructor.
+ */
 @CheckReturnValue
-inline fun CompletableObserver.disposeOn(provider: LifecycleProvider<Any>): CompletableObserver
-    = AutoDispose.completable(provider).around(this)
+inline fun <T> Maybe<T>.autoDisposeWith(scope: Maybe<*>): MaybeSubscribeProxy<T>
+    = this.to(MaybeScoper<T>(scope))
 
+/**
+ * Extension that proxies to [Completable.to] + [CompletableScoper]'s [Maybe] constructor.
+ */
 @CheckReturnValue
-inline fun <T> Subscriber<T>.cancelOn(provider: LifecycleProvider<Any>): Subscriber<T>
-    = AutoDispose.flowable(provider).around(this)
+inline fun Completable.autoDisposeWith(scope: Maybe<*>): CompletableSubscribeProxy
+    = this.to(CompletableScoper(scope))
 
+/**
+ * Extension that proxies to [Flowable.to] + [FlowableScoper]'s [ScopeProvider] constructor.
+ */
 @CheckReturnValue
-inline fun <T> Observer<T>.disposeOn(lifecycle: Maybe<Any>): Observer<T>
-    = AutoDispose.observable(lifecycle).around(this)
+inline fun <T> Flowable<T>.autoDisposeWith(provider: ScopeProvider): FlowableSubscribeProxy<T>
+    = this.to(FlowableScoper<T>(provider))
 
+/**
+ * Extension that proxies to [Observable.to] + [ObservableScoper]'s [ScopeProvider] constructor.
+ */
 @CheckReturnValue
-inline fun <T> SingleObserver<T>.disposeOn(lifecycle: Maybe<Any>): SingleObserver<T>
-    = AutoDispose.single(lifecycle).around(this)
+inline fun <T> Observable<T>.autoDisposeWith(provider: ScopeProvider): ObservableSubscribeProxy<T>
+    = this.to(ObservableScoper<T>(provider))
 
+/**
+ * Extension that proxies to [Single.to] + [SingleScoper]'s [ScopeProvider] constructor.
+ */
 @CheckReturnValue
-inline fun <T> MaybeObserver<T>.disposeOn(lifecycle: Maybe<Any>): MaybeObserver<T>
-    = AutoDispose.maybe(lifecycle).around(this)
+inline fun <T> Single<T>.autoDisposeWith(provider: ScopeProvider): SingleSubscribeProxy<T>
+    = this.to(SingleScoper<T>(provider))
 
+/**
+ * Extension that proxies to [Maybe.to] + [MaybeScoper]'s [ScopeProvider] constructor.
+ */
 @CheckReturnValue
-inline fun CompletableObserver.disposeOn(lifecycle: Maybe<Any>): CompletableObserver
-    = AutoDispose.completable(lifecycle).around(this)
+inline fun <T> Maybe<T>.autoDisposeWith(provider: ScopeProvider): MaybeSubscribeProxy<T>
+    = this.to(MaybeScoper<T>(provider))
 
+/**
+ * Extension that proxies to [Completable.to] + [CompletableScoper]'s [ScopeProvider] constructor.
+ */
 @CheckReturnValue
-inline fun <T> Subscriber<T>.cancelOn(lifecycle: Maybe<Any>): Subscriber<T>
-    = AutoDispose.flowable(lifecycle).around(this)
+inline fun Completable.autoDisposeWith(provider: ScopeProvider): CompletableSubscribeProxy
+    = this.to(CompletableScoper(provider))
 
-inline fun <T> Flowable<T>.subscribeAutoDispose(provider: LifecycleProvider<Any>,
-    onNext: Consumer<in T> = emptyConsumer(),
-    onError: Consumer<in Throwable> = AutoDisposeUtil.DEFAULT_ERROR_CONSUMER,
-    onComplete: Action = AutoDisposeUtil.EMPTY_ACTION,
-    onSubscribe: Consumer<in Subscription> = AutoDisposeUtil.EMPTY_SUBSCRIPTION_CONSUMER)
-    = subscribe(AutoDispose.flowable(provider).around(onNext, onError, onComplete, onSubscribe))
+/**
+ * Extension that proxies to [Flowable.to] + [FlowableScoper]'s [LifecycleScopeProvider]
+ * constructor.
+ */
+@CheckReturnValue
+inline fun <T> Flowable<T>.autoDisposeWith(
+    provider: LifecycleScopeProvider<*>): FlowableSubscribeProxy<T>
+    = this.to(FlowableScoper<T>(provider))
 
-inline fun <T> Observable<T>.subscribeAutoDispose(provider: LifecycleProvider<Any>,
-    onNext: Consumer<in T> = emptyConsumer(),
-    onError: Consumer<in Throwable> = AutoDisposeUtil.DEFAULT_ERROR_CONSUMER,
-    onComplete: Action = AutoDisposeUtil.EMPTY_ACTION,
-    onSubscribe: Consumer<in Disposable> = AutoDisposeUtil.EMPTY_DISPOSABLE_CONSUMER)
-    = subscribe(AutoDispose.observable(provider).around(onNext, onError, onComplete, onSubscribe))
+/**
+ * Extension that proxies to [Observable.to] + [ObservableScoper]'s [LifecycleScopeProvider]
+ * constructor.
+ */
+@CheckReturnValue
+inline fun <T> Observable<T>.autoDisposeWith(
+    provider: LifecycleScopeProvider<*>): ObservableSubscribeProxy<T>
+    = this.to(ObservableScoper<T>(provider))
 
-inline fun <T> Maybe<T>.subscribeAutoDispose(provider: LifecycleProvider<Any>,
-    onSuccess: Consumer<in T> = emptyConsumer(),
-    onError: Consumer<in Throwable> = AutoDisposeUtil.DEFAULT_ERROR_CONSUMER,
-    onComplete: Action = AutoDisposeUtil.EMPTY_ACTION,
-    onSubscribe: Consumer<in Disposable> = AutoDisposeUtil.EMPTY_DISPOSABLE_CONSUMER)
-    = subscribe(AutoDispose.maybe(provider).around(onSuccess, onError, onComplete, onSubscribe))
+/**
+ * Extension that proxies to [Single.to] + [SingleScoper]'s [LifecycleScopeProvider] constructor.
+ */
+@CheckReturnValue
+inline fun <T> Single<T>.autoDisposeWith(provider: LifecycleScopeProvider<*>): SingleSubscribeProxy<T>
+    = this.to(SingleScoper<T>(provider))
 
-inline fun <T> Single<T>.subscribeAutoDispose(provider: LifecycleProvider<Any>,
-    biConsumer: BiConsumer<in T, in Throwable>)
-    = subscribe(AutoDispose.single(provider).around(biConsumer))
+/**
+ * Extension that proxies to [Maybe.to] + [MaybeScoper]'s [LifecycleScopeProvider] constructor.
+ */
+@CheckReturnValue
+inline fun <T> Maybe<T>.autoDisposeWith(provider: LifecycleScopeProvider<*>): MaybeSubscribeProxy<T>
+    = this.to(MaybeScoper<T>(provider))
 
-inline fun <T> Single<T>.subscribeAutoDispose(provider: LifecycleProvider<Any>,
-    onSuccess: Consumer<in T> = emptyConsumer(),
-    onError: Consumer<in Throwable> = AutoDisposeUtil.DEFAULT_ERROR_CONSUMER,
-    onSubscribe: Consumer<in Disposable> = AutoDisposeUtil.EMPTY_DISPOSABLE_CONSUMER)
-    = subscribe(AutoDispose.single(provider).around(onSuccess, onError, onSubscribe))
+/**
+ * Extension that proxies to [Completable.to] + [CompletableScoper]'s [LifecycleScopeProvider]
+ * constructor.
+ */
+@CheckReturnValue
+inline fun Completable.autoDisposeWith(provider: LifecycleScopeProvider<*>): CompletableSubscribeProxy
+    = this.to(CompletableScoper(provider))
 
-inline fun Completable.subscribeAutoDispose(provider: LifecycleProvider<Any>,
-    onComplete: Action = AutoDisposeUtil.EMPTY_ACTION,
-    onError: Consumer<in Throwable> = AutoDisposeUtil.DEFAULT_ERROR_CONSUMER,
-    onSubscribe: Consumer<in Disposable> = AutoDisposeUtil.EMPTY_DISPOSABLE_CONSUMER)
-    = subscribe(AutoDispose.completable(provider).around(onComplete, onError, onSubscribe))
-
-inline fun <T> Flowable<T>.subscribeAutoDispose(lifecycle: Maybe<Any>,
-    onNext: Consumer<in T> = emptyConsumer(),
-    onError: Consumer<in Throwable> = AutoDisposeUtil.DEFAULT_ERROR_CONSUMER,
-    onComplete: Action = AutoDisposeUtil.EMPTY_ACTION,
-    onSubscribe: Consumer<in Subscription> = AutoDisposeUtil.EMPTY_SUBSCRIPTION_CONSUMER)
-    = subscribe(AutoDispose.flowable(lifecycle).around(onNext, onError, onComplete, onSubscribe))
-
-inline fun <T> Observable<T>.subscribeAutoDispose(lifecycle: Maybe<Any>,
-    onNext: Consumer<in T> = emptyConsumer(),
-    onError: Consumer<in Throwable> = AutoDisposeUtil.DEFAULT_ERROR_CONSUMER,
-    onComplete: Action = AutoDisposeUtil.EMPTY_ACTION,
-    onSubscribe: Consumer<in Disposable> = AutoDisposeUtil.EMPTY_DISPOSABLE_CONSUMER)
-    = subscribe(AutoDispose.observable(lifecycle).around(onNext, onError, onComplete, onSubscribe))
-
-inline fun <T> Maybe<T>.subscribeAutoDispose(lifecycle: Maybe<Any>,
-    onSuccess: Consumer<in T> = emptyConsumer(),
-    onError: Consumer<in Throwable> = AutoDisposeUtil.DEFAULT_ERROR_CONSUMER,
-    onComplete: Action = AutoDisposeUtil.EMPTY_ACTION,
-    onSubscribe: Consumer<in Disposable> = AutoDisposeUtil.EMPTY_DISPOSABLE_CONSUMER)
-    = subscribe(AutoDispose.maybe(lifecycle).around(onSuccess, onError, onComplete, onSubscribe))
-
-inline fun <T> Single<T>.subscribeAutoDispose(lifecycle: Maybe<Any>,
-    biConsumer: BiConsumer<in T, in Throwable>)
-    = subscribe(AutoDispose.single(lifecycle).around(biConsumer))
-
-inline fun <T> Single<T>.subscribeAutoDispose(lifecycle: Maybe<Any>,
-    onSuccess: Consumer<in T> = emptyConsumer(),
-    onError: Consumer<in Throwable> = AutoDisposeUtil.DEFAULT_ERROR_CONSUMER,
-    onSubscribe: Consumer<in Disposable> = AutoDisposeUtil.EMPTY_DISPOSABLE_CONSUMER)
-    = subscribe(AutoDispose.single(lifecycle).around(onSuccess, onError, onSubscribe))
-
-inline fun Completable.subscribeAutoDispose(lifecycle: Maybe<Any>,
-    onComplete: Action = AutoDisposeUtil.EMPTY_ACTION,
-    onError: Consumer<in Throwable> = AutoDisposeUtil.DEFAULT_ERROR_CONSUMER,
-    onSubscribe: Consumer<in Disposable> = AutoDisposeUtil.EMPTY_DISPOSABLE_CONSUMER)
-    = subscribe(AutoDispose.completable(lifecycle).around(onComplete, onError, onSubscribe))
