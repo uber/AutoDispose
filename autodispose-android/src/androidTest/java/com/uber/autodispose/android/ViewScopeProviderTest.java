def4f6bde0968046a5c87470dd06185ba5ced9b5
Zac Sweers <pandanomic@gmail.com>
2017-05-06 16:04:20 -0700

    Ignore wonky test

--- autodispose-android/src/androidTest/java/com/uber/autodispose/android/ViewScopeProviderTest.java
+++ autodispose-android/src/androidTest/java/com/uber/autodispose/android/ViewScopeProviderTest.java
@@ -27,6 +27,7 @@ import com.uber.autodispose.OutsideLifecycleException;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.subjects.PublishSubject;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -86,7 +87,8 @@ import static com.google.common.truth.Truth.assertThat;
     d.dispose();
   }
 
-  @Test public void observable_offMainThread_shouldFail() {
+  @Test @Ignore("Temporarily ignored until https://github.com/uber/AutoDispose/issues/67")
+  public void observable_offMainThread_shouldFail() {
     RecordingObserver<Integer> o = new RecordingObserver<>();
     PublishSubject<Integer> subject = PublishSubject.create();
 
