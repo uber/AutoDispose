dc23e2131728fa39ce9ce5de0ee48518e7b2567e
Zac Sweers <pandanomic@gmail.com>
2017-05-13 18:20:31 -0700

    Clean up qualifiers

--- autodispose-provided/src/main/java/com/uber/autodispose/internal/PackageNonNull.java
+++ autodispose-provided/src/main/java/com/uber/autodispose/internal/PackageNonNull.java
@@ -17,14 +17,9 @@ import javax.annotation.meta.TypeQualifierDefault;
 @Documented
 @Nonnull
 @TypeQualifierDefault({
-    ElementType.ANNOTATION_TYPE,
-    ElementType.CONSTRUCTOR,
     ElementType.FIELD,
-    ElementType.LOCAL_VARIABLE,
     ElementType.METHOD,
-    ElementType.PACKAGE,
-    ElementType.PARAMETER,
-    ElementType.TYPE
+    ElementType.PARAMETER
 })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PackageNonNull { }
