ce0d55526602ffb528dc1984ca63207fb0ab75b1
Zac Sweers <pandanomic@gmail.com>
2017-05-16 15:37:55 -0700

    More nullability annotation work (#70)
    * More nullability annotation work
    
    This is a followup to #69, and improves it in two main ways:
    - This adds it to subpackages as well, which I missed in the first pass.
    - Switches to a custom `PackageNonNull` annotation instead, which expands the checks to also include return types, fields, local vars, etc. It's pretty broad right now but we could reduce scope if need be. This is pulled into a separate artifact for re-use, but still only applied as compileOnly.
    
    * Clean up qualifiers
    
    * Tweak name
    
    * Don't export artifact
    
    Not necessary, we'll make this a separate project later
    
    * CLASS retention instead of runtime

--- autodispose-android/src/main/java/com/uber/autodispose/android/package-info.java
+++ autodispose-android/src/main/java/com/uber/autodispose/android/package-info.java
@@ -15,9 +15,8 @@
  */
 
 /**
- * AutoDispose is an RxJava 2 tool for automatically binding the execution of RxJava 2 streams to a
- * provided scope via disposal/cancellation.
+ * Android components for AutoDispose.
  */
 @com.uber.autodispose.internal.EverythingNonNullByDefault
-package com.uber.autodispose;
+package com.uber.autodispose.android;
 
