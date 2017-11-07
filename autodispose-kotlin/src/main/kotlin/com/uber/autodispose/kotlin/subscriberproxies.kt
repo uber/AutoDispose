499e7d57246d9018f71afe5c1d7e6df3bddfaf92
AJ <aj@alt>
2017-11-07 23:42:51 +0000

    Add subscribeBy extensions for proxy classes.
    Unfortunately, since the SubscribeProxy classes don't extend the
    observable class they proxy to, extension functions written for the
    observable class can't be used.
    
    One commonly used set of extensions is RxKotlin's subscribeBy. This
    commit adds copies of those extensions that work on SubscribeProxies.

---
+++ autodispose-kotlin/src/main/kotlin/com/uber/autodispose/kotlin/subscriberproxies.kt
@@ -0,0 +1,54 @@
+package com.uber.autodispose.kotlin
+
+import com.uber.autodispose.*
+import io.reactivex.disposables.Disposable
+import io.reactivex.exceptions.OnErrorNotImplementedException
+import io.reactivex.plugins.RxJavaPlugins
+
+private val onNextStub: (Any) -> Unit = {}
+private val onErrorStub: (Throwable) -> Unit = { RxJavaPlugins.onError(OnErrorNotImplementedException(it)) }
+private val onCompleteStub: () -> Unit = {}
+
+
+/**
+ * Overloaded subscribe function that allows passing named parameters
+ */
+fun <T : Any> ObservableSubscribeProxy<T>.subscribeBy(
+        onError: (Throwable) -> Unit = onErrorStub,
+        onComplete: () -> Unit = onCompleteStub,
+        onNext: (T) -> Unit = onNextStub
+): Disposable = subscribe(onNext, onError, onComplete)
+
+/**
+ * Overloaded subscribe function that allows passing named parameters
+ */
+fun <T : Any> FlowableSubscribeProxy<T>.subscribeBy(
+        onError: (Throwable) -> Unit = onErrorStub,
+        onComplete: () -> Unit = onCompleteStub,
+        onNext: (T) -> Unit = onNextStub
+): Disposable = subscribe(onNext, onError, onComplete)
+
+/**
+ * Overloaded subscribe function that allows passing named parameters
+ */
+fun <T : Any> SingleSubscribeProxy<T>.subscribeBy(
+        onError: (Throwable) -> Unit = onErrorStub,
+        onSuccess: (T) -> Unit = onNextStub
+): Disposable = subscribe(onSuccess, onError)
+
+/**
+ * Overloaded subscribe function that allows passing named parameters
+ */
+fun <T : Any> MaybeSubscribeProxy<T>.subscribeBy(
+        onError: (Throwable) -> Unit = onErrorStub,
+        onComplete: () -> Unit = onCompleteStub,
+        onSuccess: (T) -> Unit = onNextStub
+): Disposable = subscribe(onSuccess, onError, onComplete)
+
+/**
+ * Overloaded subscribe function that allows passing named parameters
+ */
+fun CompletableSubscribeProxy.subscribeBy(
+        onError: (Throwable) -> Unit = onErrorStub,
+        onComplete: () -> Unit = onCompleteStub
+): Disposable = subscribe(onComplete, onError)
