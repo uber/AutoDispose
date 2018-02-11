4125e8c32b4f19a6aee9dab434694bf7b28f21e4
Zac Sweers <zac.sweers@gmail.com>
2018-02-11 01:51:13 +0000

    Update RELEASING and add update_docs.sh script

---
+++ .buildscript/update_docs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "Cloning osstrich..."
+mkdir tmp
+cd osstrich-tmp
+git clone git@github.com:square/osstrich.git
+cd osstrich
+echo "Packaging..."
+mvn package
+echo "Running..."
+rm -rf tmp/autodispose && java -jar target/osstrich-cli.jar tmp/autodispose git@github.com:uber/autodispose.git com.uber.autodispose
+echo "Cleaning up..."
+cd ../..
+rm -rf tmp
+echo "Finished!"
