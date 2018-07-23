5e1c1ed458c4f0af68903ff3f0ad45c470c934c4
Zac Sweers <pandanomic@gmail.com>
2018-07-22 23:35:19 -0700

    Simplify lifecyclescopes by removing defer handling of blocks

--- lifecycle/autodispose-lifecycle/src/main/java/com/uber/autodispose/lifecycle/LifecycleScopes.java
+++ lifecycle/autodispose-lifecycle/src/main/java/com/uber/autodispose/lifecycle/LifecycleScopes.java
@@ -16,16 +16,11 @@
 
 package com.uber.autodispose.lifecycle;
 
-import com.uber.autodispose.AutoDisposePlugins;
-import com.uber.autodispose.OutsideScopeException;
 import com.uber.autodispose.internal.ScopeEndNotification;
 import io.reactivex.Maybe;
-import io.reactivex.MaybeSource;
 import io.reactivex.Observable;
-import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
-import java.util.concurrent.Callable;
 
 /**
  * Utilities for dealing with {@link LifecycleScopeProvider}s. This includes factories for resolving
@@ -35,80 +30,40 @@ public final class LifecycleScopes {
 
   private static final Function<Object, ScopeEndNotification> TRANSFORM_TO_END =
       new Function<Object, ScopeEndNotification>() {
-        @Override public ScopeEndNotification apply(Object o) {
+        @Override
+        public ScopeEndNotification apply(Object o) {
           return ScopeEndNotification.INSTANCE;
         }
       };
 
-  private static final Predicate<Boolean> IDENTITY_BOOLEAN_PREDICATE = new Predicate<Boolean>() {
-    @Override public boolean test(Boolean b) {
-      return b;
-    }
-  };
+  private static final Predicate<Boolean> IDENTITY_BOOLEAN_PREDICATE =
+      new Predicate<Boolean>() {
+        @Override
+        public boolean test(Boolean b) {
+          return b;
+        }
+      };
 
   private LifecycleScopes() {
     throw new InstantiationError();
   }
 
   /**
-   * Overload for resolving lifecycle providers that defaults to checking start and end boundaries
-   * of lifecycles. That is, they will ensure that the lifecycle has both started and not ended.
+   * Overload for resolving lifecycle providers that defaults to checking start and end boundaries *
+   * of lifecycles. That is, they will ensure that the lifecycle has both started and not ended. *
    *
    * @param provider the {@link LifecycleScopeProvider} to resolve.
-   * @param <E> the lifecycle event type.
-   * @return a resolved {@link Maybe} representation of a given provider
-   */
-  public static <E> Maybe<?> deferredResolvedLifecycle(
-      LifecycleScopeProvider<E> provider) {
-    return deferredResolvedLifecycle(provider, true, true);
-  }
-
-  /**
-   * @param provider the {@link LifecycleScopeProvider} to resolve.
-   * @param checkStartBoundary whether or not to check that the lifecycle has started
-   * @param checkEndBoundary whether or not to check that the lifecycle has ended
    * @param <E> the lifecycle event type
    * @return a resolved {@link Maybe} representation of a given provider
    */
-  public static <E> Maybe<?> deferredResolvedLifecycle(
-      final LifecycleScopeProvider<E> provider,
-      final boolean checkStartBoundary,
-      final boolean checkEndBoundary) {
-    return Maybe.defer(new Callable<MaybeSource<?>>() {
-      @Override public MaybeSource<?> call() throws Exception {
-        E lastEvent = provider.peekLifecycle();
-        if (checkStartBoundary && lastEvent == null) {
-          LifecycleNotStartedException exception = new LifecycleNotStartedException();
-          Consumer<? super OutsideScopeException> handler
-              = AutoDisposePlugins.getOutsideScopeHandler();
-          if (handler != null) {
-            handler.accept(exception);
-            return Maybe.just(ScopeEndNotification.INSTANCE);
-          } else {
-            throw exception;
-          }
-        }
-        E endEvent;
-        try {
-          endEvent = provider.correspondingEvents()
-              .apply(lastEvent);
-        } catch (Exception e) {
-          if (checkEndBoundary && e instanceof LifecycleEndedException) {
-            Consumer<? super OutsideScopeException> handler
-                = AutoDisposePlugins.getOutsideScopeHandler();
-            if (handler != null) {
-              handler.accept((LifecycleEndedException) e);
-              return Maybe.just(ScopeEndNotification.INSTANCE);
-            } else {
-              throw e;
-            }
-          } else {
-            return Maybe.error(e);
-          }
-        }
-        return resolveScopeFromLifecycle(provider.lifecycle(), endEvent);
-      }
-    });
+  public static <E> Maybe<?> resolveScopeFromLifecycle(final LifecycleScopeProvider<E> provider)
+      throws Exception {
+    E lastEvent = provider.peekLifecycle();
+    if (lastEvent == null) {
+      throw new LifecycleNotStartedException();
+    }
+    E endEvent = provider.correspondingEvents().apply(lastEvent);
+    return resolveScopeFromLifecycle(provider.lifecycle(), endEvent);
   }
 
   /**
@@ -117,25 +72,29 @@ public final class LifecycleScopes {
    * @param <E> the lifecycle event type
    * @return a resolved {@link Maybe} representation of a given lifecycle, targeting the given event
    */
-  public static <E> Maybe<?> resolveScopeFromLifecycle(
-      Observable<E> lifecycle,
-      final E endEvent) {
+  public static <E> Maybe<?> resolveScopeFromLifecycle(Observable<E> lifecycle, final E endEvent) {
     Function<E, Boolean> equalityFunction;
     if (endEvent instanceof Comparable) {
       //noinspection unchecked
-      equalityFunction = (Function<E, Boolean>) new Function<Comparable<E>, Boolean>() {
-        @Override public Boolean apply(Comparable<E> e) {
-          return e.compareTo(endEvent) >= 0;
-        }
-      };
+      equalityFunction =
+          (Function<E, Boolean>)
+              new Function<Comparable<E>, Boolean>() {
+                @Override
+                public Boolean apply(Comparable<E> e) {
+                  return e.compareTo(endEvent) >= 0;
+                }
+              };
     } else {
-      equalityFunction = new Function<E, Boolean>() {
-        @Override public Boolean apply(E e) {
-          return e.equals(endEvent);
-        }
-      };
+      equalityFunction =
+          new Function<E, Boolean>() {
+            @Override
+            public Boolean apply(E e) {
+              return e.equals(endEvent);
+            }
+          };
     }
-    return lifecycle.skip(1)
+    return lifecycle
+        .skip(1)
         .map(equalityFunction)
         .filter(IDENTITY_BOOLEAN_PREDICATE)
         .map(TRANSFORM_TO_END)
