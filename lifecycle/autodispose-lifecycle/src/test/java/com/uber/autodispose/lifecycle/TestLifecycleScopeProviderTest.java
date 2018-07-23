5242e081d0b9706c0bcd595113a13a5d49a9d763
Zac Sweers <pandanomic@gmail.com>
2018-07-22 18:02:07 -0700

    Move TestLifecycleScopeProviderTest to new artifact

--- lifecycle/autodispose-lifecycle/src/test/java/com/uber/autodispose/lifecycle/TestLifecycleScopeProviderTest.java
+++ lifecycle/autodispose-lifecycle/src/test/java/com/uber/autodispose/lifecycle/TestLifecycleScopeProviderTest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2017. Uber Technologies
+ * Copyright (C) 2018. Uber Technologies
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,13 +14,13 @@
  * limitations under the License.
  */
 
-package com.uber.autodispose;
+package com.uber.autodispose.lifecycle;
 
 import org.junit.Test;
 
 import static com.google.common.truth.Truth.assertThat;
-import static com.uber.autodispose.TestLifecycleScopeProvider.TestLifecycle.STARTED;
-import static com.uber.autodispose.TestLifecycleScopeProvider.TestLifecycle.STOPPED;
+import static com.uber.autodispose.lifecycle.TestLifecycleScopeProvider.TestLifecycle.STARTED;
+import static com.uber.autodispose.lifecycle.TestLifecycleScopeProvider.TestLifecycle.STOPPED;
 
 public class TestLifecycleScopeProviderTest {
 
