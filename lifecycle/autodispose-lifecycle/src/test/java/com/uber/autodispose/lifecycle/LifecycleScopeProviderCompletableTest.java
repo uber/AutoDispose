6bb7e3ea9258d094f252ef95429ade63c35d84e1
Zac Sweers <pandanomic@gmail.com>
2018-07-22 18:14:37 -0700

    Move LifecycleScopeProvider portions of tests to new artifact

---
+++ lifecycle/autodispose-lifecycle/src/test/java/com/uber/autodispose/lifecycle/LifecycleScopeProviderCompletableTest.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2018. Uber Technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.uber.autodispose.lifecycle;
+
+import com.uber.autodispose.AutoDisposePlugins;
+import com.uber.autodispose.RxErrorsRule;
+import com.uber.autodispose.test.RecordingObserver;
+import io.reactivex.Completable;
+import io.reactivex.subjects.BehaviorSubject;
+import io.reactivex.subjects.CompletableSubject;
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.uber.autodispose.AutoDispose.autoDisposable;
+import static com.uber.autodispose.lifecycle.TestUtil.makeLifecycleProvider;
+
+public class LifecycleScopeProviderCompletableTest {
+
+  private static final RecordingObserver.Logger LOGGER = new RecordingObserver.Logger() {
+    @Override public void log(String message) {
+      System.out.println(
+          LifecycleScopeProviderCompletableTest.class.getSimpleName() + ": " + message);
+    }
+  };
+
+  @Rule public RxErrorsRule rule = new RxErrorsRule();
+
+  @After public void resetPlugins() {
+    AutoDisposePlugins.reset();
+  }
+
+  @Test public void autoDispose_withLifecycleProvider_completion() {
+    RecordingObserver<Integer> o = new RecordingObserver<>(LOGGER);
+    CompletableSubject source = CompletableSubject.create();
+    BehaviorSubject<Integer> lifecycle = BehaviorSubject.createDefault(0);
+    LifecycleScopeProvider<Integer> provider = makeLifecycleProvider(lifecycle);
+    source.as(autoDisposable(provider))
+        .subscribe(o);
+    o.takeSubscribe();
+
+    assertThat(source.hasObservers()).isTrue();
+    assertThat(lifecycle.hasObservers()).isTrue();
+
+    lifecycle.onNext(1);
+
+    assertThat(source.hasObservers()).isTrue();
+    assertThat(lifecycle.hasObservers()).isTrue();
+
+    source.onComplete();
+    o.assertOnComplete();
+
+    o.assertNoMoreEvents();
+    assertThat(source.hasObservers()).isFalse();
+    assertThat(lifecycle.hasObservers()).isFalse();
+  }
+
+  @Test public void autoDispose_withLifecycleProvider_interrupted() {
+    RecordingObserver<Integer> o = new RecordingObserver<>(LOGGER);
+    CompletableSubject source = CompletableSubject.create();
+    BehaviorSubject<Integer> lifecycle = BehaviorSubject.createDefault(0);
+    LifecycleScopeProvider<Integer> provider = makeLifecycleProvider(lifecycle);
+    source.as(autoDisposable(provider))
+        .subscribe(o);
+    o.takeSubscribe();
+
+    assertThat(source.hasObservers()).isTrue();
+    assertThat(lifecycle.hasObservers()).isTrue();
+
+    lifecycle.onNext(1);
+
+    assertThat(source.hasObservers()).isTrue();
+    assertThat(lifecycle.hasObservers()).isTrue();
+
+    lifecycle.onNext(3);
+
+    // All disposed
+    assertThat(source.hasObservers()).isFalse();
+    assertThat(lifecycle.hasObservers()).isFalse();
+
+    // No one is listening
+    source.onComplete();
+    o.assertNoMoreEvents();
+  }
+
+  @Test public void autoDispose_withLifecycleProvider_withoutStartingLifecycle_shouldFail() {
+    BehaviorSubject<Integer> lifecycle = BehaviorSubject.create();
+    RecordingObserver<Integer> o = new RecordingObserver<>(LOGGER);
+    LifecycleScopeProvider<Integer> provider = makeLifecycleProvider(lifecycle);
+    Completable.complete()
+        .as(autoDisposable(provider))
+        .subscribe(o);
+
+    o.takeSubscribe();
+    assertThat(o.takeError()).isInstanceOf(LifecycleNotStartedException.class);
+  }
+
+  @Test public void autoDispose_withLifecycleProvider_afterLifecycle_shouldFail() {
+    BehaviorSubject<Integer> lifecycle = BehaviorSubject.createDefault(0);
+    lifecycle.onNext(1);
+    lifecycle.onNext(2);
+    lifecycle.onNext(3);
+    RecordingObserver<Integer> o = new RecordingObserver<>(LOGGER);
+    LifecycleScopeProvider<Integer> provider = makeLifecycleProvider(lifecycle);
+    Completable.complete()
+        .as(autoDisposable(provider))
+        .subscribe(o);
+
+    o.takeSubscribe();
+    assertThat(o.takeError()).isInstanceOf(LifecycleEndedException.class);
+  }
+
+}
---
+++ lifecycle/autodispose-lifecycle/src/test/java/com/uber/autodispose/lifecycle/LifecycleScopeProviderSingleTest.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2018. Uber Technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.uber.autodispose.lifecycle;
+
+import com.uber.autodispose.AutoDispose;
+import com.uber.autodispose.AutoDisposePlugins;
+import com.uber.autodispose.RxErrorsRule;
+import com.uber.autodispose.test.RecordingObserver;
+import io.reactivex.Single;
+import io.reactivex.subjects.BehaviorSubject;
+import io.reactivex.subjects.SingleSubject;
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.uber.autodispose.lifecycle.TestUtil.makeLifecycleProvider;
+
+public class LifecycleScopeProviderSingleTest {
+
+  private static final RecordingObserver.Logger LOGGER = new RecordingObserver.Logger() {
+    @Override public void log(String message) {
+      System.out.println(
+          LifecycleScopeProviderSingleTest.class.getSimpleName() + ": " + message);
+    }
+  };
+
+  @Rule public RxErrorsRule rule = new RxErrorsRule();
+
+  @After public void resetPlugins() {
+    AutoDisposePlugins.reset();
+  }
+
+  @Test public void autoDispose_withLifecycleProvider() {
+    RecordingObserver<Integer> o = new RecordingObserver<>(LOGGER);
+    SingleSubject<Integer> source = SingleSubject.create();
+    BehaviorSubject<Integer> lifecycle = BehaviorSubject.createDefault(0);
+    LifecycleScopeProvider<Integer> provider = makeLifecycleProvider(lifecycle);
+    source.as(AutoDispose.<Integer>autoDisposable(provider))
+        .subscribe(o);
+    o.takeSubscribe();
+
+    assertThat(source.hasObservers()).isTrue();
+    assertThat(lifecycle.hasObservers()).isTrue();
+
+    lifecycle.onNext(1);
+
+    assertThat(source.hasObservers()).isTrue();
+    assertThat(lifecycle.hasObservers()).isTrue();
+
+    source.onSuccess(3);
+    o.takeSuccess();
+
+    // All cleaned up
+    o.assertNoMoreEvents();
+    assertThat(source.hasObservers()).isFalse();
+    assertThat(lifecycle.hasObservers()).isFalse();
+  }
+
+  @Test public void autoDispose_withLifecycleProvider_interrupted() {
+    RecordingObserver<Integer> o = new RecordingObserver<>(LOGGER);
+    SingleSubject<Integer> source = SingleSubject.create();
+    BehaviorSubject<Integer> lifecycle = BehaviorSubject.createDefault(0);
+    LifecycleScopeProvider<Integer> provider = makeLifecycleProvider(lifecycle);
+    source.as(AutoDispose.<Integer>autoDisposable(provider))
+        .subscribe(o);
+    o.takeSubscribe();
+
+    assertThat(source.hasObservers()).isTrue();
+    assertThat(lifecycle.hasObservers()).isTrue();
+
+    lifecycle.onNext(1);
+
+    assertThat(source.hasObservers()).isTrue();
+    assertThat(lifecycle.hasObservers()).isTrue();
+
+    // Lifecycle ends
+    lifecycle.onNext(3);
+    assertThat(source.hasObservers()).isFalse();
+    assertThat(lifecycle.hasObservers()).isFalse();
+
+    // No one is listening even if upstream finally does emit
+    source.onSuccess(3);
+    o.assertNoMoreEvents();
+  }
+
+  @Test public void autoDispose_withProvider_withoutStartingLifecycle_shouldFail() {
+    BehaviorSubject<Integer> lifecycle = BehaviorSubject.create();
+    RecordingObserver<Integer> o = new RecordingObserver<>(LOGGER);
+    LifecycleScopeProvider<Integer> provider = makeLifecycleProvider(lifecycle);
+    Single.just(1)
+        .as(AutoDispose.<Integer>autoDisposable(provider))
+        .subscribe(o);
+
+    o.takeSubscribe();
+    assertThat(o.takeError()).isInstanceOf(LifecycleNotStartedException.class);
+  }
+
+  @Test public void autoDispose_withProvider_afterLifecycle_shouldFail() {
+    BehaviorSubject<Integer> lifecycle = BehaviorSubject.createDefault(0);
+    lifecycle.onNext(1);
+    lifecycle.onNext(2);
+    lifecycle.onNext(3);
+    RecordingObserver<Integer> o = new RecordingObserver<>(LOGGER);
+    LifecycleScopeProvider<Integer> provider = makeLifecycleProvider(lifecycle);
+    Single.just(1)
+        .as(AutoDispose.<Integer>autoDisposable(provider))
+        .subscribe(o);
+
+    o.takeSubscribe();
+    assertThat(o.takeError()).isInstanceOf(LifecycleEndedException.class);
+  }
+
+}
