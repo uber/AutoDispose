63e0212eb154f1c7478ca97e9d0071878c670eec
Zac Sweers <pandanomic@gmail.com>
2018-07-22 17:53:56 -0700

    Make LifecycleScopeProvider extend ScopeProvider

--- lifecycle/autodispose-lifecycle/src/main/java/com/uber/autodispose/lifecycle/LifecycleScopeProvider.java
+++ lifecycle/autodispose-lifecycle/src/main/java/com/uber/autodispose/lifecycle/LifecycleScopeProvider.java
@@ -16,35 +16,36 @@
 
 package com.uber.autodispose.lifecycle;
 
+import com.uber.autodispose.ScopeProvider;
 import com.uber.autodispose.internal.DoNotMock;
+import io.reactivex.Maybe;
 import io.reactivex.Observable;
 import io.reactivex.annotations.CheckReturnValue;
 import io.reactivex.functions.Function;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * An interface that, when implemented, provides information to AutoDispose to allow it to resolve
- * the next lifecycle event and construct a Maybe representation of it from the {@link #lifecycle()}
- * stream.
+ * A convenience interface that, when implemented, helps provide information to create {@link
+ * ScopeProvider} implementations that resolve the next corresponding lifecycle event and construct
+ * a {@link Maybe} representation of it from the {@link #lifecycle()} stream.
+ *
+ * <p>Convenience resolver utilities for this can be found in {@link LifecycleScopes}.
  *
  * @param <E> the lifecycle event type.
+ * @see LifecycleScopes
  */
 @DoNotMock(value = "Use TestLifecycleScopeProvider instead")
-public interface LifecycleScopeProvider<E> {
+public interface LifecycleScopeProvider<E> extends ScopeProvider {
 
-  /**
-   * @return a sequence of lifecycle events.
-   */
+  /** @return a sequence of lifecycle events. */
   @CheckReturnValue Observable<E> lifecycle();
 
   /**
    * @return a sequence of lifecycle events. It's recommended to back this with a static instance to
-   * avoid unnecessary object allocation.
+   *     avoid unnecessary object allocation.
    */
   @CheckReturnValue Function<E, E> correspondingEvents();
 
-  /**
-   * @return the last seen lifecycle event, or {@code null} if none.
-   */
+  /** @return the last seen lifecycle event, or {@code null} if none. */
   @Nullable E peekLifecycle();
 }
