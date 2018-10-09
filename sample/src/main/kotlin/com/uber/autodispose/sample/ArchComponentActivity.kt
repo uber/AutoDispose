df939037d9eec51179da23b0993ec04be98bd2d0
Zac Sweers <zac.sweers@gmail.com>
2018-10-09 23:49:09 +0200

    Add missing import

--- sample/src/main/kotlin/com/uber/autodispose/sample/ArchComponentActivity.kt
+++ sample/src/main/kotlin/com/uber/autodispose/sample/ArchComponentActivity.kt
@@ -25,6 +25,7 @@ import androidx.lifecycle.ViewModelProviders
 import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider
 import com.uber.autodispose.autoDisposable
 import com.uber.autodispose.sample.repository.ImageRepository
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 class ArchComponentActivity: AppCompatActivity() {
