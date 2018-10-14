327da40294b0b14863e8f5bc4f55cd402e23eaab
Shaishav Gandhi <shaishgandhi@gmail.com>
2018-10-14 16:13:42 -0600

    Add animal-sniffer to guard against Java 8 only APIs (#264)
    * Add animal-sniffer to guard against Java 8 only APIs
    
    Signed-off-by: shaishavgandhi05 <shaishgandhi@gmail.com>
    
    * Add Java 8 Only API: ZonedDateTime
    
    Signed-off-by: shaishavgandhi05 <shaishgandhi@gmail.com>
    
    * Revert "Add animal-sniffer to guard against Java 8 only APIs"
    
    This reverts commit 21187290c4eed399ea0fa95fe4633a77cd64d491.

---
+++ autodispose/src/test/java/com/uber/autodispose/TestZonedDateTime.java
@@ -0,0 +1,11 @@
+package com.uber.autodispose;
+
+import java.time.ZonedDateTime;
+import org.junit.Test;
+
+public class TestZonedDateTime {
+
+  @Test public void testJava8Signature() {
+    ZonedDateTime dateTime = ZonedDateTime.now();
+  }
+}
