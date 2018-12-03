9276702618beb53936da4cec376d238a8d54ded6
Shaishav Gandhi <shaishgandhi@gmail.com>
2018-12-02 22:49:37 -0800

    Add link to lint-dev discussion for further clarity

--- static-analysis/autodispose-lint/src/main/kotlin/com/uber/autodispose/lint/AutoDisposeDetector.kt
+++ static-analysis/autodispose-lint/src/main/kotlin/com/uber/autodispose/lint/AutoDisposeDetector.kt
@@ -51,6 +51,8 @@ class AutoDisposeDetector: Detector(), SourceCodeScanner {
         // JAVA and TEST_SOURCES in the `scope` parameter but by providing the `analysisScopes`
         // params, we're indicating that this check can run on either JAVA or TEST_SOURCES and
         // doesn't require both of them together.
+        // From discussion on lint-dev https://groups.google.com/d/msg/lint-dev/ULQMzW1ZlP0/1dG4Vj3-AQAJ
+        // TODO: Remove after AGP 3.4 release when this behavior will no longer be required.
         Implementation(AutoDisposeDetector::class.java,
             EnumSet.of(Scope.JAVA_FILE, Scope.TEST_SOURCES),
             EnumSet.of(Scope.JAVA_FILE),
