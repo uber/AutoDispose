0c66c51bf9333a93d9de258d02e8e68797056559
Zac Sweers <zac.sweers@gmail.com>
2019-09-12 04:33:00 -0400

    Stub tests

---
+++ autodispose-coroutines-interop/src/test/kotlin/com/uber/autodispose/coroutinesintrop/AutoDisposeCoroutinesInterop.kt
@@ -0,0 +1,9 @@
+package com.uber.autodispose.coroutinesintrop
+
+class AutoDisposeCoroutinesInterop {
+  // TODO Each rx type extension
+  // TODO ScopeProvider -> Scope
+  // TODO Completable -> Scope
+  // TODO Scope -> ScopeProvider
+  // TODO Scope -> Completable
+}
