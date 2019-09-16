2a507f528a7df1387dad7833767c11faffed7a40
Zac Sweers <zac.sweers@gmail.com>
2019-09-15 20:13:37 -0400

    Add PlugingsMatrixTest for testing multiple configs

---
+++ autodispose/src/test/java/com/uber/autodispose/PluginsMatrixTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2019. Uber Technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.uber.autodispose;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public abstract class PluginsMatrixTest {
+
+  @Parameterized.Parameters
+  public static Collection primeNumbers() {
+    return Arrays.asList(new Object[][] {{true}, {false}});
+  }
+
+  protected final boolean hideProxies;
+
+  public PluginsMatrixTest(boolean hideProxies) {
+    this.hideProxies = hideProxies;
+  }
+
+  @Before
+  public void setUp() {
+    AutoDisposePlugins.setHideProxies(hideProxies);
+  }
+
+  @After
+  public void tearDown() {
+    AutoDisposePlugins.reset();
+  }
+}
