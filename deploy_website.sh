f745f3c3e2158975da08383d815e01ab746f1911
Shaishav Gandhi <shaishav@uber.com>
2019-12-09 12:03:35 -0800

    Commit 1.x Docs (#411)
    * Commit 1.x Docs
    
    Since these aren't generated anymore, we'll commit them for the site generator to link them
    
    * Generate 1.x docs on the fly based on tag
    
    * Try changing to API 16 emulator
    
    * Try removing extra info
    
    * Try removing Gradle options
    
    * Try travis_wait
    
    * Bring back OOM
    
    * Try instructions from Travis
    
    https://docs.travis-ci.com/user/languages/android/
    
    * Try workaround for Travis
    
    * Gah start emulator after zip stuff

--- deploy_website.sh
+++ deploy_website.sh
@@ -22,7 +22,17 @@ if ! [ $local ]; then
   # Move working directory into temp folder
   cd $DIR
 
-  # Generate the API docs
+  # Fetch all tags
+  git fetch --all --tags --prune
+
+  # Checkout the last 1.x release
+  git checkout tags/1.4.0
+
+  # Generate the 1.x docs
+  ./gradlew dokka
+
+  git checkout master
+  # Generate the API docs for 2.x
   ./gradlew dokka
 fi
 
