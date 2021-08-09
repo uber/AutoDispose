a8e575d83c1b2316f3987e16eb2dce27043b849d
Zac Sweers <pandanomic@gmail.com>
2021-08-09 15:05:27 -0500

    Deployed 8bd9a35 with MkDocs version: 1.1.2

---
+++ 2.x/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":android:autodispose-androidx-lifecycle:dokkaHtml/androidTestRelease":[],":android:autodispose-androidx-lifecycle:dokkaHtml/debug":[],":android:autodispose-androidx-lifecycle:dokkaHtml/main":[],":android:autodispose-androidx-lifecycle:dokkaHtml/release":[]}'
