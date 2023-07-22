88099eb5c87d2be5931de0770b2a15c105cf42c1
Zac Sweers <pandanomic@gmail.com>
2023-07-22 17:06:54 -0400

    Replace http:// with https:// repo-wide

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -190,7 +190,7 @@ subprojects {
           suppressGeneratedFiles.set(true)
           suppressInheritedMembers.set(true)
           externalDocumentationLink {
-            url.set(URI("http://reactivex.io/RxJava/3.x/javadoc/").toURL())
+            url.set(URI("https://reactivex.io/RxJava/3.x/javadoc/").toURL())
           }
           externalDocumentationLink {
             url.set(URI("https://kotlin.github.io/kotlinx.coroutines/index.html").toURL())
